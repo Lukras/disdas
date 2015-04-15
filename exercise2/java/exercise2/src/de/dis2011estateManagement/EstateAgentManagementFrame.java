38faf25a9c682c31419337b2d06a00a2a610e452
Philipp <4bonfigt@informatik.uni-hamburg.de>
2015-04-15 13:30:35 +0200

    exercise12

--- exercise2/java/exercise2/src/de/dis2011estateManagement/EstateAgentManagementFrame.java
+++
@@ -1,8 +0,0 @@
-package de.dis2011estateManagement;
-
-import javax.swing.JFrame;
-
-public class EstateAgentManagementFrame extends JFrame {
-	private static final long serialVersionUID = -6990942376290020635L;
-	
-}

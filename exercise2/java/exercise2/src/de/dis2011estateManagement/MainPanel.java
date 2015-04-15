5d12cd0afb491fe6ac6d045f529d1dd3ac3be568
Philipp <4bonfigt@informatik.uni-hamburg.de>
2015-04-15 16:20:45 +0200

    exercise12

---
+++ exercise2/java/exercise2/src/de/dis2011estateManagement/MainPanel.java
@@ -0,0 +1,37 @@
+package de.dis2011estateManagement;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel implements ActionListener {
+
+	private static final long serialVersionUID = -7091238205226400082L;
+	
+	private JButton btnEstateAgentMgmt;
+	
+	private MainFrame mainFrame;
+	
+	public MainPanel(MainFrame mainFrame) {
+		super();
+		this.mainFrame = mainFrame;
+		
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		btnEstateAgentMgmt = new JButton("Estate Agent Management");
+		btnEstateAgentMgmt.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnEstateAgentMgmt.addActionListener(this);
+		this.add(btnEstateAgentMgmt);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnEstateAgentMgmt) {
+			mainFrame.goToEstateAgentLoginPanel();
+		}
+	}
+}
